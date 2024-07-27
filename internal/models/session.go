c94309c703338fd3d27ef34e47013b91cad93258
Wahyu Asta <whyasta@gmail.com>
2024-07-27 23:34:45 +0700

    update session name

--- internal/models/session.go
+++ internal/models/session.go
@@ -5,6 +5,7 @@ import "time"
 type Session struct {
 	ID           int64     `gorm:"column:id; primary_key; not null" json:"id"`
 	EventID      int64     `gorm:"column:event_id" mapstructure:"event_id" json:"event_id" validate:"required"`
+	Sessioname   string    `gorm:"column:session_name" mapstructure:"session_name" json:"session_name" validate:"required,min=5,max=100"`
 	SessionStart time.Time `gorm:"column:session_start" mapstructure:"session_start" json:"session_start"`
 	SessionEnd   time.Time `gorm:"column:session_end" mapstructure:"session_end" json:"session_end"`
 	CommonModel
