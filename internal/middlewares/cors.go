19d67ca4721abeb74d2b004014555f220dde6244
Wahyu Asta <wahyu.asta@bigmind.id>
2024-08-03 18:51:12 +0700

    update cors

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -1,19 +1,16 @@
 package middlewares
 
 import (
-	"time"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-	/*return func(c *gin.Context) {
+	return func(c *gin.Context) {
 
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
@@ -21,15 +18,15 @@ func CORSMiddleware() gin.HandlerFunc {
 		}
 
 		c.Next()
-	}*/
+	}
 
-	return cors.New(cors.Config{
-		// AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"POST", "GET", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-		AllowCredentials: true,
-		MaxAge:           1 * time.Hour * 24,
-		AllowAllOrigins:  true,
-	})
+	// return cors.New(cors.Config{
+	// 	// AllowOrigins:     []string{"*"},
+	// 	AllowMethods:     []string{"POST", "GET", "OPTIONS"},
+	// 	AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
+	// 	ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           1 * time.Hour * 24,
+	// 	AllowAllOrigins:  true,
+	// })
 }
