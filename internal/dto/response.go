7596cb559010e35ac185ef16ca9e17b64f0b9e39
Wahyu Asta <wahyu.asta@bigmind.id>
2024-09-05 08:21:41 +0700

    update

--- internal/dto/response.go
+++ internal/dto/response.go
@@ -2,7 +2,7 @@ package dto
 
 type ResponseDto struct {
 	ID       int64    `gorm:"column:id; primary_key; not null" json:"id" mapstructure:"id"`
-	GateName string   `gorm:"column:gate_name"  mapstructure:"gate_name" json:"gate_name" validate:"required,min=3,max=20"`
+	GateName string   `gorm:"column:gate_name"  mapstructure:"gate_name" json:"gate_name" validate:"required,min=3,max=100"`
 	EventID  int64    `gorm:"column:event_id"  mapstructure:"event_id" json:"event_id" validate:"required"`
 	Users    []*int64 `gorm:"column:users"  mapstructure:"users" json:"users"`
 }
--- internal/models/gate.go
+++ internal/models/gate.go
@@ -2,7 +2,7 @@ package models
 
 type Gate struct {
 	ID       int64  `gorm:"column:id; primary_key; not null" json:"id" mapstructure:"id"`
-	GateName string `gorm:"column:gate_name"  mapstructure:"gate_name" json:"gate_name" validate:"required,min=3,max=20"`
+	GateName string `gorm:"column:gate_name"  mapstructure:"gate_name" json:"gate_name" validate:"required,min=3,max=100"`
 	EventID  int64  `gorm:"column:event_id"  mapstructure:"event_id" json:"event_id,omitempty" validate:"required"`
 	Users    []User `gorm:"many2many:gate_users;" json:"users,omitempty"`
 	CommonModel
